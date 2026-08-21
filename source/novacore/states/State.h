e990b7ad1c7084a43a7ca6ff6afe851973103184
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-08-21 14:45:04 -0300

    Enhance State class with Pause, Resume, and accessors
    Added Pause and Resume methods, and new accessors for transparency and update blocking.

--- source/novacore/states/State.h
+++ source/novacore/states/State.h
@@ -8,6 +8,18 @@ public:
 
     virtual void Enter() {}
     virtual void Exit() {}
+
+    virtual void Pause() {}
+    virtual void Resume() {}
+
+    virtual void HandleEvent(const SDL_Event& event) { (void)event; }
     virtual void Update(float deltaTime) { (void)deltaTime; }
     virtual void Render(SDL_Renderer* renderer) { (void)renderer; }
+
+    bool IsTransparent() const { return transparent; }
+    bool BlocksUpdate() const { return blocksUpdate; }
+
+protected:
+    bool transparent = false;
+    bool blocksUpdate = false;
 };
