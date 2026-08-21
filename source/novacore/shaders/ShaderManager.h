293780671323725077c0c38a87926d70670098fc
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-08-21 05:27:00 -0300

    Add ShaderManager class for shader management

---
+++ source/novacore/shaders/ShaderManager.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <string>
+#include <unordered_map>
+
+struct Shader {
+    GLuint program = 0;
+};
+
+class ShaderManager {
+public:
+    static void Init();
+    static void Shutdown();
+
+    static bool LoadShader(const std::string& name, const std::string& vertexPath, const std::string& fragmentPath);
+    static void UnloadShader(const std::string& name);
+    static void ClearAll();
+
+    static void Use(const std::string& name);
+    static void Unuse();
+
+    static GLuint GetProgram(const std::string& name);
+    static bool HasShader(const std::string& name);
+
+    static void SetUniformInt(const std::string& name, const std::string& uniform, int value);
+    static void SetUniformFloat(const std::string& name, const std::string& uniform, float value);
+    static void SetUniformVec2(const std::string& name, const std::string& uniform, float x, float y);
+    static void SetUniformVec3(const std::string& name, const std::string& uniform, float x, float y, float z);
+    static void SetUniformVec4(const std::string& name, const std::string& uniform, float x, float y, float z, float w);
+    static void SetUniformMat4(const std::string& name, const std::string& uniform, const float* matrix);
+
+private:
+    static GLuint CompileStage(GLenum type, const std::string& source);
+    static std::string ReadFile(const std::string& path);
+    static GLint GetUniformLocation(GLuint program, const std::string& uniform);
+
+    static std::unordered_map<std::string, Shader> shaderCache;
+    static std::string activeShader;
+};
