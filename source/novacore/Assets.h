5a12e26252e8e845179cf66b9c98ed8b7f1e8de7
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-08-21 05:06:14 -0300

    Add sound and music management to Assets class

--- source/novacore/Assets.h
+++ source/novacore/Assets.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
 #include <string>
 #include <unordered_map>
 
@@ -11,9 +12,27 @@ public:
 
     static SDL_Texture* LoadTexture(const std::string& path);
     static void UnloadTexture(const std::string& path);
+
+    static Mix_Chunk* LoadSound(const std::string& path);
+    static void UnloadSound(const std::string& path);
+
+    static Mix_Music* LoadMusic(const std::string& path);
+    static void UnloadMusic(const std::string& path);
+
+    static void PreloadTextures(const std::vector<std::string>& paths);
+
+    static void ClearTextures();
+    static void ClearSounds();
+    static void ClearMusic();
     static void ClearAll();
+
+    static size_t TextureCount();
+    static size_t SoundCount();
+    static size_t MusicCount();
 
 private:
     static SDL_Renderer* renderer;
     static std::unordered_map<std::string, SDL_Texture*> textureCache;
+    static std::unordered_map<std::string, Mix_Chunk*> soundCache;
+    static std::unordered_map<std::string, Mix_Music*> musicCache;
 };
