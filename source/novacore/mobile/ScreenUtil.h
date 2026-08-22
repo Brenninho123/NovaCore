247a507a75ad81a197e3948d8bc9370235fadfa0
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-08-22 07:07:27 -0300

    Add new methods for screen fitting and touch handling

--- source/novacore/mobile/ScreenUtil.h
+++ source/novacore/mobile/ScreenUtil.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <SDL2/SDL.h>
+#include <functional>
 
 enum class Orientation {
     Landscape,
@@ -31,12 +32,32 @@ public:
     static void SetSafeArea(const SafeArea& area);
 
     static float ToDesignSpace(float pixelValue, float designReferenceHeight);
+
     static SDL_Rect FitToScreen(int contentWidth, int contentHeight);
+    static SDL_Rect FitToScreenInteger(int contentWidth, int contentHeight);
+    static int GetIntegerScale(int contentWidth, int contentHeight);
+
+    static bool IsMultiTouchActive();
+    static int GetActiveTouchCount();
+
+    static void SetOnOrientationChanged(std::function<void(Orientation)> callback);
+    static void SetOnSafeAreaChanged(std::function<void(const SafeArea&)> callback);
+
+    static void HandleEvent(const SDL_Event& event);
 
 private:
+    static void NotifyOrientationIfChanged();
+
     static SDL_Window* window;
     static int width;
     static int height;
     static float dpi;
     static SafeArea safeArea;
+    static Orientation lastOrientation;
+
+    static int activeFingerCount;
+    static SDL_FingerID activeFingers[8];
+
+    static std::function<void(Orientation)> onOrientationChanged;
+    static std::function<void(const SafeArea&)> onSafeAreaChanged;
 };
